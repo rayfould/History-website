52bf06b73d611fb63a6e2cf2019701487afcbdba
rudolfwr <rudolfr@bu.edu>
2024-05-03 00:56:25 -0400

    Added back to top button to every page

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("backToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("backToTopBtn").style.display = "none";
+    }
+}
+
+document.getElementById("backToTopBtn").addEventListener("click", function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
