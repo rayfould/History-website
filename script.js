04c387cd30d533adc756a87cd39d79e09d1e9242
rudolfwr <rudolfr@bu.edu>
2024-06-22 06:25:15 -0400

    Updated button references, allowed button to bring back to main page

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("backToTopBtn").style.display = "block";
-    } else {
-        document.getElementById("backToTopBtn").style.display = "none";
-    }
-}
+document.getElementById("backToTopBtn").style.display = "block";
 
 document.getElementById("backToTopBtn").addEventListener("click", function() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    window.location.href = 'index.html';
 });
